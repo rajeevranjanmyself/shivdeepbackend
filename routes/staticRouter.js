260786ac5836bdb551178dc796c19da1d2414ff8
rajeevranjanmyself <rajeevranjan.myself@gmail.com>
2025-02-27 18:27:15 +0530

    tet

--- routes/staticRouter.js
+++ routes/staticRouter.js
@@ -5,6 +5,9 @@ const router = express.Router();
 router.get("/privacy-policy", async (req, res) => {
   return res.render("privacypolicy");
 });
+router.get("/terms-conditions", async (req, res) => {
+  return res.render("termsconditions");
+});
 
 
 module.exports = router;
